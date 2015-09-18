0e5d382306681d53aba42fbeea9677988d059864
f0x <f0x.asavin@gmail.com>
2015-09-18 09:15:50 -0400

    sky_scan.py beta added

---
+++ python_lib/sky_scan.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python
+#v0.1
+
+from optparse import OptionParser
+import time
+import json
+
+import turret
+from pythonwifi import iwlibs
+
+def median(lst):
+	lst = sorted([x for x in lst if x is not None])
+	if (len(lst) < 1):
+		return None
+	if ((len(lst) % 2) == 1):
+		return lst[((len(lst)+1)/2)-1]
+	else:
+		return float(sum(lst[(len(lst)/2)-1:(len(lst)/2)+1]))/2.0
+
+def uniq(seq):
+    seen = set()
+    seen_add = seen.add
+    return [ x for x in seq if not (x in seen or seen_add(x))]
+
+##### Options #####
+parser = OptionParser()
+parser.add_option("-p", "--port",dest="tty",
+		help="Serial port of connected Arduino servo controller", metavar='tty')
+parser.add_option("-i", "--iface",dest="iface",
+		help="Interface that will be used for scanning", metavar='iface')
+##mode: scan find
+##verbose
+##outfile
+##starty, finy
+##startx, finx
+##delta
+
+##### defaults #####
+tty="/dev/ttyUSB0"
+iface="wlan0"
+startx=0
+finx=180
+starty=0
+finy=180
+delta=60
+num_measures=3
+measures_sleep=1
+
+(options, args) = parser.parse_args()
+turret=turret.MyTurret(tty)
+wifi=iwlibs.Wireless(iface)
+
+curx=startx
+
+sky={}
+all_aps=[]
+
+### X azimut
+while True: 
+	if curx > finx: curx=finx
+	sky[curx]={}
+	turret.setx(curx)
+	print 'x'+str(curx)
+
+### Y Elevation
+	cury=starty
+	while True: 
+		if cury > finy: cury=finy
+		turret.sety(cury)
+		print 'y'+str(cury)
+		sky[curx][cury]={}
+		## scan
+		scan_results={}
+		aps_bssid=[]
+		for m in range(0, num_measures):
+			scan_results[m]={}
+			results=wifi.scan()
+			for i in results:
+				scan_results[m][i.bssid]=i.quality.siglevel
+				aps_bssid.append(i.bssid)
+				all_aps.append(i.bssid)
+			time.sleep(measures_sleep)
+		aps_bssid=uniq(aps_bssid)
+	
+		### sort and calculate ###
+	
+		for ap in aps_bssid:
+			power=[]
+			for m in range(0, num_measures):
+				if ap in scan_results[m]:
+					power.append(scan_results[m][ap])
+			sky[curx][cury][ap]=median(power)
+		if cury==finy: break
+		cury += delta
+	## DEBUG
+	#break
+		
+	if curx==finx: break
+	curx += delta
+all_aps=uniq(all_aps)
+
+#print json.dumps(sky,sort_keys=True)
+#print sky
+
+maximum={}
+for ap in all_aps:
+	maximum[ap]={}
+	appower=0
+	for cx in sky:
+		for cy in sky[cx]:
+			if ap in sky[cx][cy]: 
+				if sky[cx][cy][ap] > appower:
+					appower=sky[cx][cy][ap]
+					maximum[ap]['x']=cx
+					maximum[ap]['y']=cy
+					maximum[ap]['level']=sky[cx][cy][ap]
+					maximum[ap]['dbm']=sky[cx][cy][ap]-256
+print maximum
--- python_lib/test_wifi.py
+++ python_lib/test_wifi.py
@@ -5,7 +5,12 @@ iface=iwlibs.Wireless('wlan0')
 
 #print dir (iface)
 scanresults=iface.scan()
+a={}
 
 for i in scanresults:
 	print i.bssid+"   "+str(i.quality.siglevel-256)
+	a[i.bssid] = i.quality.siglevel
 #	print dir(i.range)
+
+for bssid,level in a.items():
+	print bssid+": "+str(level)
